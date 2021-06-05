96918dad4c600c36e6d75bb90a7c3018887de9ef
Valentin <vf59165@hotmail.com>
2021-06-05 21:52:44 +0200

    je_continue_de_commenter_2

--- controller/Logout.php
+++ controller/Logout.php
@@ -1,17 +1,17 @@
 <?php
-  require_once(__dir__ . '/Controller.php');
+  require_once(__dir__ . '/Controller.php'); //requiert le dossier du fichier et controller.php une seule fois chacun
 
-  class Logout extends Controller {
+  class Logout extends Controller { //on crée la classe Logout qui s'étend dans la classe Controller
 
     /**
-      * @param null|void
-      * @return null|void
+      * @param null|void //définit les paramètres comment étant null ou vide
+      * @return null|void //retourne des valeurs soit null soit vide
       * @desc 
     **/
-    public function __construct()
+    public function __construct() // on crée le constructeur de notre classe
     {
-      session_destroy();
-      header("Location: index.php");
+      session_destroy(); //on utilise la fonction php pour terminet une session
+      header("Location: index.php"); //on est ensuite redirigé sur index.php
     }
   }
  ?>
